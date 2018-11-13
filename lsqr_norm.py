f89a592b5950162fceb72df2526e5cef01f658d1
Yi Liu <yiliu@calicolabs.com>
2018-11-13 15:24:45 -0800

    breaking; bn shape; affine opt

--- lsqr_norm.py
+++ lsqr_norm.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+# import absl # todo use absl.logging
 
 def weighted_update(var, new, momentum=.9, return_assigned_value=False, recast=True, assign_name=None):
     recast_dtype = new.dtype
@@ -16,21 +16,57 @@ def weighted_update(var, new, momentum=.9, return_assigned_value=False, recast=T
     else:
         return assign_op
 
+def lsqrn(x, Gr, BHWC=None, name_dict=None, training=False, momentum=.99, data_format='channels_last', 
+          epsilon=1e-5, l2_regularizer=1e-3, affine_channels='afterall'):
+    """
+    Parameters:
+    ===========
+    x: tf.Tensor()
+        input float tensor known shape BHWC or BCHW, depending on data_format
+    BHWC: [int, int, int int]
+        shape of x_BHWC. Must all be positive integers
+    Gr: [int, int]
+        G is the number of groups. G must divide C
+        r is the number of residual channels to compute for each group. r must be less than or equal to G
+    TODO: Support i = 0 or r = 0
+    """
+    assert affine_channels in ['indep', 'afterall']
+    if data_format == 'channels_last':
+        x_BHWC = tf.identity(x)
+    elif data_format == 'channels_first': # BCHW -> BHWC
+        x_BHWC = tf.transpose(x, [0, 2, 3, 1])
+    else:
+        raise ValueError()
 
-def lsqrn(x_BHWC, BHWC, Gr, name_dict=None, training=False, momentum=.9):
-    # todo, let B be -1
-    #G = 4
-    #r = 3
-    [B, H, W, C] = BHWC
+    if BHWC is not None:
+        [B, H, W, C] = BHWC
+    else:
+        x_shape = tf.shape(x_BHWC)
+        x_shape_list = x_BHWC.get_shape().as_list()
+        B = x_shape[0]
+        H = x_shape[1]
+        W = x_shape[2]
+        C = x_shape_list[3] 
+
     [G, r] = Gr
     c = C // G 
     
     i = c - r
     j = i + 1
-    assert i > 0
 
     if name_dict is None:
-        name_dict = {'lsq_beta': 'lsq_beta'}
+        name_dict = {'lsq_beta': 'lsq_beta',
+                     'affine_b': 'affine_b',
+                     'affine_c': 'affine_c'}
+    print(name_dict['lsq_beta'], [G, j, r])
+    print('lsqr_norm: i = {}. r = {}.'.format(i, r))
+    if i <= 0 or r <= 0:
+        print("reverting to basic BN")
+        return tf.layers.batch_normalization(
+            x_BHWC, axis=3, training=training, epsilon=epsilon,
+            fused=True, renorm=False,
+            ), {'name_dict': name_dict}
+    assert i > 0
     var_lsq_beta_Gjr = tf.get_variable(
         name=name_dict['lsq_beta'], 
         dtype=tf.float64, 
@@ -40,9 +76,14 @@ def lsqrn(x_BHWC, BHWC, Gr, name_dict=None, training=False, momentum=.9):
 
     
     x_BHWGc = tf.reshape(x_BHWC, [B, H, W, G, c]) 
-    x_BHWGr = x_BHWGc[:, :, :, :, :r]
+    x_BHWGr = x_BHWGc[:, :, :, :, :r] # the first r channels are to have their residuals taken
     x_BHWGi = x_BHWGc[:, :, :, :, r:]
-    z_BHWGi = tf.layers.batch_normalization(x_BHWGi, axis=4,  training=training)
+    x_BHW_Gi = tf.reshape(x_BHWGi, [B, H, W, G * i])
+    z_BHW_Gi = tf.layers.batch_normalization(x_BHW_Gi, axis=3, training=training,
+        epsilon=epsilon, fused=True, renorm=False,
+        center=affine_channels=='indep',
+        scale=affine_channels=='indep')
+    z_BHWGi = tf.reshape(z_BHW_Gi, [B, H, W, G, i])
     z_BHWGj = tf.concat([z_BHWGi, tf.ones([B, H, W, G, 1], dtype=tf.float32)], axis=4)
 
     # hat matrix:  X (X* X)^-1 X*
@@ -50,25 +91,48 @@ def lsqrn(x_BHWC, BHWC, Gr, name_dict=None, training=False, momentum=.9):
     # lsq_beta: (X* X)^-1 X* y
     z_G_BHW_j = tf.reshape(tf.transpose(z_BHWGj, [3, 0, 1, 2, 4]), [G, B * H * W, j])
     x_G_BHW_r = tf.reshape(tf.transpose(x_BHWGr, [3, 0, 1, 2, 4]), [G, B * H * W, r])
-    curr_lsq_beta_Gjr = tf.linalg.lstsq(z_G_BHW_j, x_G_BHW_r)
-    update_beta = weighted_update(var_lsq_beta_Gjr, curr_lsq_beta_Gjr, momentum = momentum, return_assigned_value=False,
+    curr_lsq_beta_Gjr = tf.linalg.lstsq(z_G_BHW_j, x_G_BHW_r, fast=True, l2_regularizer=l2_regularizer)
+    update_beta = weighted_update(var_lsq_beta_Gjr, curr_lsq_beta_Gjr,
+                                  momentum = momentum, return_assigned_value=False,
                                   recast=True, assign_name='assign_beta')
     if training:
         lsq_beta_Gjr = curr_lsq_beta_Gjr
         tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, update_beta)
     else:
-        lsq_beta_Gjr = var_lsq_beta_Gjr
+        lsq_beta_Gjr = tf.cast(var_lsq_beta_Gjr, tf.float32)
 
     yhat_BHWGr = tf.einsum('bhwgj,gjr->bhwgr', z_BHWGj , lsq_beta_Gjr)
     residuals_BHWGr = x_BHWGr - yhat_BHWGr
-    tensors={
+    z_BHWGc = tf.concat([residuals_BHWGr, z_BHWGi], axis=4)
+    z_BHWC = tf.reshape(z_BHWGc, [B, H, W, C])
+    helper_dict={
         'x_BHWGc': x_BHWGc,
         'x_BHWGr': x_BHWGr,
         'x_BHWGi': x_BHWGi,
-        'x_BHWGj': x_BHWGj,
+        'x_BHW_Gi': x_BHW_Gi,
+        'z_BHWGj': z_BHWGj,
+        'z_BHW_Gi': z_BHW_Gi, 
+        'residuals_BHWGr': residuals_BHWGr,
+        'z_BHWGc': z_BHWGc,
+        'z_BHWC': z_BHWC,
+        'name_dict': name_dict}
 
-    }
-    helper_dict = dict(name_dict=name_dict)
-    return residuals_BHWGr, helper_dict
+    if affine_channels == 'afterall':
+        var_affine_b = tf.get_variable(
+            name=name_dict['affine_b'],  dtype=tf.float32,  shape=[C], 
+            initializer=tf.zeros_initializer(), trainable=True)
+        var_affine_c = tf.get_variable(
+            name=name_dict['affine_c'],  dtype=tf.float32,  shape=[C], 
+            initializer=tf.ones_initializer(), trainable=True)
+        z_BHWC = z_BHWC * var_affine_c + var_affine_b
+
+    if data_format == 'channels_last':
+        z_out = tf.identity(z_BHWC)
+    elif data_format == 'channels_first': # BHWC -> BCHW  
+        z_out = tf.transpose(z_BHWC, [0, 3, 1, 2])
+    else:
+        raise ValueError()
+
+    return z_out, helper_dict
 
     
--- test_lsqr_norm.py
+++ test_lsqr_norm.py
@@ -2,10 +2,12 @@ import unittest
 import tensorflow as tf
 import numpy as np
 import lsqr_norm
+
+
 class TestStringMethods(unittest.TestCase):
 
         
-    def test_lsqrn():
+    def test_lsqrn(self,):
         B, H, W, C = [2, 11, 13, 28]
         G, r = 4, 3
         G = 4
@@ -16,38 +18,54 @@ class TestStringMethods(unittest.TestCase):
         training = True
         momentum = .9
         print("ijcr:", i, j, c, r)
+
         assert c * G == C
 
         x_BHWC = tf.placeholder(shape=[B, H, W, C], dtype=tf.float32)
         x_np_rand = np.random.random([B, H, W, C])
-        residuals_BHWGr, helper_dict = lsqr_norm.lsqrn(x_BHWC, BHWC=[B, H, W, C], Gr=[G, r], 
-            name_dict=None, training=training, momentum=momentum)
+        z_BHWC, helper_dict = lsqr_norm.lsqrn(x_BHWC, Gr=[G, r], BHWC=[B, H, W, C],
+            name_dict = {'lsq_beta': 'lsq_beta0'}, training=training, momentum=momentum,
+            data_format='channel_last')
+        # two from layers batch normalization, one from our update
+        assert len(tf.get_collection(tf.GraphKeys.UPDATE_OPS)) == 3
+
+        z1_BCHW, helper_dict = lsqr_norm.lsqrn(tf.transpose(x_BHWC, [0, 3, 1, 2]), 
+            Gr=[G, r], BHWC=[B, H, W, C],
+            name_dict = {'lsq_beta': 'lsq_beta1'}, training=training, momentum=momentum,
+            data_format='channels_first')
+        z1_BHWC = tf.transpose(z1_BCHW, [0, 2, 3, 1])
+
         x_BHWGr = helper_dict['x_BHWGr']
         x_BHWGi = helper_dict['x_BHWGi']
+        residuals_BHWGr = helper_dict['residuals_BHWGr']
         # x_BHWGj = helper_dict['x_BHWGj']
 
         s = tf.Session()
         init_all = tf.global_variables_initializer()
         s.run(init_all)
-        np_resids = s.run(residuals_BHWGr, {x_BHWC: x_np_rand})
+        feed_dict = {x_BHWC: x_np_rand}
+        np_resids = s.run(residuals_BHWGr, feed_dict=feed_dict)
         np_x_BHWGr = s.run(x_BHWGr, {x_BHWC: x_np_rand})
-        print(np.linalg.norm(np_resids), np.linalg.norm(np_x_BHWGr))
+        assert np.linalg.norm(np_resids) < np.linalg.norm(np_x_BHWGr)
         
         # attempt to collect gradients
         loss = tf.reduce_sum(residuals_BHWGr ** 2)
         gradients_x = tf.gradients(loss, x_BHWC)
         gradients_x_BHWGr = tf.gradients(loss, x_BHWGr)
         gradients_x_BHWGi = tf.gradients(loss, x_BHWGi)
-        np_resids = s.run([gradients_x, gradients_x_BHWGi, gradients_x_BHWGr], {x_BHWC: x_np_rand}) # attempt to get gradients
+        np_resids = s.run([gradients_x, gradients_x_BHWGi, gradients_x_BHWGr],
+                          {x_BHWC: x_np_rand}) # attempt to get gradients
 
-        # two from layers batch normalization, one from our update
-        assert len(tf.get_collection(tf.GraphKeys.UPDATE_OPS)) == 3
+        
         
         # try to run a train opo
         optimizer = tf.train.GradientDescentOptimizer(0.01)
         train_op = optimizer.minimize(loss)
-        s.run(train_op)
+        s.run(train_op, feed_dict=feed_dict)
 
+        zshape0, zshape1 = s.run([tf.shape(z_BHWC), tf.shape(z1_BHWC)])
+        print(zshape0, zshape1)
+        assert np.all(zshape0 == zshape1)
 
 
 if __name__ == '__main__':
